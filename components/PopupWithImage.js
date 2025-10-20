51ec0a8edead2f44e3f6f882c016c0fb060919aa
Renato <renato.rfco@gmail.com>
2025-10-20 16:01:27 +0200

    Adicionar classes Popup, PopupWithImage, PopWithForm, UserInfo

---
+++ components/PopupWithImage.js
@@ -0,0 +1,16 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._image = this._popup.querySelctor(".photoFrame__image");
+    this._caption = this._popup.querySelector(".photoFrame__text");
+  }
+
+  open(name, link) {
+    this._image.src = link;
+    this._image.alt = name;
+    this._caption.textContent = name;
+    super.open();
+  }
+}
